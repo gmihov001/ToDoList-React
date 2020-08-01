0fcfb3643f43da7d0c5e020c34afb20d9d3cfc7e
George Mihov <gmihov001@gmail.com>
2020-08-01 19:15:51 +0000

    updates

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,34 +1,44 @@
 import React from "react";
 
 export class Home extends React.Component {
+	//input field with enter key submission
+	//array to hold tasks
+	//map the array - for each element, display it in an <li>
+	//add delete functionality - with event listener onclick
+	//delete icon - to show on hover
+	//when no tasks: display "No tasks, add a task"
 
-    //input field with enter key submission
-    //array to hold tasks
-    //map the array - for each element, display it in an <li>
-    //add delete functionality - with event listener onclick
-    //delete icon - to show on hover
-    //when no tasks: display "No tasks, add a task"
+	constructor() {
+		super();
+		this.state = {
+			tasks: ["First task", "Second task"]
+		};
+	}
 
-    
-    constructor(){
-        super();
-        this.state = {
-            tasks: ["First task", "Second task"]
-        };
-    }
+	saveTask = e => {
+		console.log(e);
+	};
 
-    render(){
-        var { tasks } = this.state;
-	return (
-		<div className="text-center mt-5">
-            <h1>TO DO:</h1>
-			<input type="text" placeholder="What needs to be done?"/>
-            <ul>
-                {tasks.map(task => {
-                <li>{task}</li>
-                })}
-            </ul>
-		</div>
-    );
-    }
+	render() {
+		var { tasks } = this.state;
+		return (
+			<div className="text-center mt-5">
+				<h1>TO DO:</h1>
+				<input
+					type="text"
+					placeholder="What needs to be done?"
+					onKeyUp={this.saveTask}
+				/>
+				<ul>
+					{tasks.map(task => {
+						return (
+							<li>
+								{task} <span> X </span>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		);
+	}
 }
