b6ca4662e4123728a73d9b6cae24eeabcd9312f3
George Mihov <gmihov001@gmail.com>
2020-08-01 19:10:15 +0000

    updates

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,28 +1,33 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 export class Home extends React.Component {
 
-    //input field 
+    //input field with enter key submission
     //array to hold tasks
-    //map the array - for each element, display in an <li>
-    //add delete functionality
+    //map the array - for each element, display it in an <li>
+    //add delete functionality - with event listener onclick
+    //delete icon - to show on hover
+    //when no tasks: display "No tasks, add a task"
+
     
     constructor(){
         super();
         this.state = {
-
+            tasks: ["First task", "Second task"]
         };
     }
 
     render(){
+        var { tasks } = this.state;
 	return (
 		<div className="text-center mt-5">
             <h1>TO DO:</h1>
-			
+			<input type="text" placeholder="What needs to be done?"/>
+            <ul>
+                {tasks.map(task => {
+                <li>{task}</li>
+                })}
+            </ul>
 		</div>
     );
     }
